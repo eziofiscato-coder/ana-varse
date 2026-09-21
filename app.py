45d5db32c31dcdf545f2fbfbc63e8dd6d3603e45
eziofiscato-coder <ezio.fiscato@gmail.com>
2026-09-21 14:29:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def hdr():
     with a:
         try:
             if os.path.exists("copertina.png"):
-                st.image("copertina.png",width=70)
+                st.image("copertina.png",width=50)
             elif os.path.exists("logo.png"):
                 st.image("logo.png",width=10)
             else:
